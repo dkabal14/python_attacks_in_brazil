cafb7a79ca2cce7a1ddf7b930a490a1ecdf8b515
Diego R. Sousa <diegorosariosousa@gmail.com>
2023-11-17 15:39:35 -0300

    Convertendo Json para xlsx

--- Json-to-XLSX/Json_to_XLSX.py
+++ Json-to-XLSX/Json_to_XLSX.py
@@ -3,11 +3,20 @@ import json
 import pandas as pd
 import openpyxl
 
+def Json_to_XLSX(importFile):
+    import json
+    import pandas as pd
+    
+    try:
+        data = json.load(open(importFile))
+        df = pd.DataFrame(data['data'])
+        filename = 'export.xlsx'
+        with pd.ExcelWriter(filename, engine='openpyxl') as xlWriter:
+            df.to_excel(xlWriter,sheet_name='json', index=False)
+        print(f'criado arquivo {filename}')
+        sys.exit(0)
+    except:
+        sys.exit(1)
+
 importFile = sys.argv[1]
-
-data = json.load(open(importFile))
-
-df = pd.DataFrame(data['data'])
-
-with pd.ExcelWriter('export.xlsx', engine='openpyxl') as xlWriter:
-    df.to_excel(xlWriter,sheet_name='json', index=False)
+Json_to_XLSX(importFile)
--- Json-to-XLSX/test-call.py
+++ Json-to-XLSX/test-call.py
@@ -1,6 +1,19 @@
 import sys
 import subprocess
 
-jsonFile = 'C:/Users/Trabalho/VSCode_GIT_Workspaces/python_attacks_in_brazil/Json-to-XLSX/files/Sign_offs_1.json'
+liLibs = [
+    'openpyxl',
+    'pandas'
+]
+
+for lib in liLibs:
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--no-warn-script-location', '--disable-pip-version-check', '--no-warn-script-location', '--no-warn-conflicts', '--quiet', lib])
+    except:
+        print(f"Não foi possível atualizar a biblioteca {lib}")
+
+    
+jsonFile = 'C:/users/Trabalho/JSON_TESTE.json'
 jsonScript = 'C:/Users/Trabalho/VSCode_GIT_Workspaces/python_attacks_in_brazil/Json-to-XLSX/Json_to_XLSX.py'
-subprocess.check_call([sys.executable, jsonScript, jsonFile])
+Response = subprocess.check_call([sys.executable, jsonScript, jsonFile])
+sys.exit(Response)
